5316ae8895286f17229f80640ae98f07777fa969
Marc Maninang <marc.maninang@gmail.com>
2017-08-07 21:32:23 -0700

    Fixed for actual movies and release dates

--- TheBasics/ex_3.rb
+++ TheBasics/ex_3.rb
@@ -1,6 +1,6 @@
 my_hash = {"The Matrix": 1999,
-           "King Arther": 2017,
-           "Fight Club": 2003,
-           "Cronk": 1975 }
+           "King Arthur": 2017,
+           "The Dark Knight": 2008,
+           "Seven": 1995 }
 
 my_hash.each { |k, v| puts v }
