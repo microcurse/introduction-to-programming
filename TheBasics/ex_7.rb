3e6deb706ffe93a67e78a75efd74608ee29328ed
Marc Maninang <marc.maninang@gmail.com>
2017-08-07 22:11:00 -0700

    Answer for exercise 7

---
+++ TheBasics/ex_7.rb
@@ -0,0 +1,6 @@
+=begin
+SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
+  from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
+
+This error tells me that the line was supposed to end with a } instead of a )
+=end
