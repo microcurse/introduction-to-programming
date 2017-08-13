74de6589ea58acbba8d336e3b65862bbdca06b9d
microcurse <marc.maninang@gmail.com>
2017-08-13 11:25:59 -0700

    finished Hashes chapter

--- Hashes/ex_5.rb
+++ Hashes/ex_5.rb
@@ -1,2 +1,10 @@
-groceries = {fruits: "Vegetable"
-             }
+burger = {meat: "Beef",
+          vegetable: "Lettuce",
+          cheese: "Pepper Jack",
+          buns: "Wheat"}
+
+if burger.has_value?("Beef")
+  puts "Yes."
+else
+  puts "No."
+end
---
+++ Hashes/ex_7.rb
@@ -0,0 +1,6 @@
+x = "hi there"
+my_hash = {x: "some value"}
+my_hash2 = {x => "some value"}
+
+# The difference is that x: uses x as the key
+# and x => uses the value of x as the key
